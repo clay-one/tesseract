5cd3e425ad9961048346efd70965e83be8a5856f
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-07-15 16:02:00 +0430

    port TagInfoController

---
+++ src/Web/Controllers/TagInfoController.cs
@@ -0,0 +1,37 @@
+﻿using Appson.Tesseract.Web.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Appson.Tesseract.Web.Controllers
+{
+    public class TagInfoController : ApiControllerBase
+    {
+        // GET	/tags/ns/:ns/list	-B, SAFE	Get list of tags in a namespace
+        // GET	/tags/ns/:ns/t/:t	-B, SAFE	Get info on a single tag, its cardinality, etc.
+
+        /// <summary>
+        /// GET
+        /// /tags/ns/:ns/list
+        /// -B, SAFE
+        /// Get list of tags in a namespace
+        /// </summary>
+        [HttpGet("tags/ns/{ns}/list")]
+        public IActionResult GetTagListInNamespace(string ns)
+        {
+            return NotImplemented();
+        }
+
+        /// <summary>
+        /// GET
+        /// /tags/ns/:ns/t/:t
+        /// -B, SAFE
+        /// Get info on a single tag, its cardinality, etc.
+        /// </summary>
+        [HttpGet("tags/ns/{ns}/t/{tag}")]
+        public IActionResult GetTagInfo(string ns, string tag)
+        {
+            return NotImplemented();
+        }
+
+
+    }
+}
