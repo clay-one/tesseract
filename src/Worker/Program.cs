0352f5ff5af3bff3b7740943b44d557a68213e2b
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-08-24 01:08:22 +0430

    Setup generic host and minimal job monitoring hosted service implementation

--- src/Worker/Program.cs
+++ src/Worker/Program.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Tesseract.Core;
+using Tesseract.Worker.Services;
+
+// ReSharper disable StringLiteralTypo
 
 namespace Tesseract.Worker
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await GetHostBuilder(args)
+                .Build()
+                .RunAsync();
+        }
+
+        private static IHostBuilder GetHostBuilder(string[] args)
+        {
+            return new HostBuilder()
+                .ConfigureHostConfiguration(configBuilder =>
+                {
+                    configBuilder
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile(@"hostsettings.json", false)
+                        .AddCommandLine(args);
+                })
+                .ConfigureAppConfiguration((hostBuilderContext, appConfigBuilder) =>
+                {
+                    var envName = hostBuilderContext.HostingEnvironment.EnvironmentName;
+                    appConfigBuilder
+                        .AddJsonFile(@"appsettings.json", false)
+                        .AddJsonFile($@"appsettings.{envName}.json", true)
+                        .AddEnvironmentVariables($@"TSRCT_{envName}")
+                        .AddCommandLine(args);
+                })
+                .ConfigureServices((hostBuilderContext, serviceCollection) =>
+                {
+                    serviceCollection.AddTesseractCoreServices();
+                    serviceCollection.AddHostedService<MonitorService>();
+                })
+                .ConfigureLogging((hostBuilderContext, loggingBuilder) =>
+                {
+                    loggingBuilder
+                        .AddConfiguration(hostBuilderContext.Configuration)
+                        .AddConsole();
+                })
+                .UseConsoleLifetime();
         }
     }
 }
